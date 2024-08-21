daa221f85453d7f06497ff98d3c08163d0c46d68
alex-yang-upenn <chukyang@sas.upenn.edu>
2024-08-20 17:13:04 -0700

    initial commit, tested on localhost

---
+++ roomie_backend/chat/serializers.py
@@ -0,0 +1,31 @@
+from rest_framework import serializers
+
+from .models import Conversation, ConversationMessage
+
+from useraccount.serializers import UserDetailSerializer
+
+
+class ConversationSerializer(serializers.ModelSerializer):
+    users = UserDetailSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Conversation
+        fields = (
+            'id',
+            'users',
+            'modified_at'
+        )
+
+class ConversationMessageSerializer(serializers.ModelSerializer):
+    sent_to = UserDetailSerializer(many=False, read_only=True)
+    created_by = UserDetailSerializer(many=False, read_only=True)
+
+    class Meta:
+        model = ConversationMessage
+        fields = (
+            'id',
+            'body',
+            'sent_to',
+            'created_by'
+        )
+
