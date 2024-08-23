f53ebd2ae4d012a0c1c53da87648d47a251d0875
alex-yang-upenn <chukyang@sas.upenn.edu>
2024-08-22 22:10:17 -0700

    making admin page static files available

--- roomie_backend/roomie_backend/urls.py
+++ roomie_backend/roomie_backend/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('api/properties/', include('property.urls')),
     path('api/auth/', include('useraccount.urls')),
     path('api/chat/', include('chat.urls'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
+  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
